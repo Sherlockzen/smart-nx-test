91675251785056c1a4140349667529ed1fa55b11
Hewerton <hewertonlourenco@gmail.com>
2023-05-27 19:50:17 -0300

    Feat: generated the initial table with the persons

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,53 @@ import { useEffect, useState } from 'react'
 
 
 function App() {
-  const [url, setUrl] = useState('https://swapi.dev/api/people/');
+  // const [url, setUrl] = useState('https://swapi.dev/api/people/');
+  const [currPage, setCurrPage] = useState(0);
+  const [list, setList] = useState({
+    count: Number,
+    next: String || null,
+    previous: String || null,
+    results: [],
+  });
+
+  const url = 'https://swapi.dev/api/people/';
 
   useEffect(() => {
-    const data = fetch(url).then(resp => resp.json())
-  })
+    fetch(url).then(resp => resp.json()).then((value) => setList(value))
+  }, [])
+
+  console.log(list);
+  
 
   return (
     <>
-      <div className=' flex flex-col items-center'>
-        <h1 className=' text-5xl'>Testando!</h1>
-        
+      <div className=" flex flex-col items-center">
+        <h1 className=" text-5xl">Testando!</h1>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row */}
+              {
+                list.results.map((person, index) => (
+                  <tr key={person.name}>
+                    <th>{index}</th>
+                    <td>{person.name}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App
