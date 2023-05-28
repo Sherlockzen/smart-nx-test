3d6c03858deb24ca495a3fb6c70f4f55cf907227
Hewerton <hewertonlourenco@gmail.com>
2023-05-27 21:47:29 -0300

    Feat: verify if its the first/last page to not go to next or previous

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 
 function App() {
   // const [url, setUrl] = useState('https://swapi.dev/api/people/');
-  const [currPage, setCurrPage] = useState(0);
+  const [currPage, setCurrPage] = useState(1);
   const [list, setList] = useState({
     count: Number,
     next: String || null,
@@ -11,20 +11,24 @@ function App() {
     results: [],
   });
 
-  const url = 'https://swapi.dev/api/people/';
-
+  const url = 'https://swapi.dev/api/people/?page=' + currPage;
+  
   useEffect(() => {
     fetch(url).then(resp => resp.json()).then((value) => setList(value))
-  }, [])
-
-  console.log(list);
+  }, [url])
+  
+  const finalPage = Math.ceil(+list.count / 10)
+  console.log(finalPage);
   
 
   return (
     <>
-      <div className=" flex flex-col items-center">
-        <h1 className=" text-5xl">Testando!</h1>
-        <div className="overflow-x-auto">
+      <div className=" flex flex-col items-center w-screen h-screen">
+        <div className="navbar bg-base-300 flex justify-center">
+          <a className="btn btn-ghost Uppercase text-3xl">Star Wars</a>
+          <div>{currPage}</div>
+        </div>
+        <div className="overflow-x-auto w-2/3">
           <table className="table w-full">
             {/* head */}
             <thead>
@@ -34,17 +38,28 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {/* row */}
-              {
-                list.results.map((person, index) => (
-                  <tr key={person.name}>
-                    <th>{index}</th>
-                    <td>{person.name}</td>
-                  </tr>
-                ))
-              }
+              {list.results.map((person: { name: string; url: string }) => (
+                <tr key={person.name} className=" hover">
+                  <th>{person.url.match(/\d+/)}</th>
+                  <td>{person.name}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
+        </div>
+        <div className="btn-group grid grid-cols-2">
+          <button
+            onClick={() => (currPage > 1 ? setCurrPage(currPage - 1) : null)}
+            className="btn btn-outline"
+          >
+            Previous page
+          </button>
+          <button
+            onClick={() => finalPage === currPage ? null : setCurrPage(currPage + 1)}
+            className="btn btn-outline"
+          >
+            Next
+          </button>
         </div>
       </div>
     </>
