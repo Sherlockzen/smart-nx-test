1849e070ed8b759cbf09f7fe96aae12c7107266b
Hewerton <hewertonlourenco@gmail.com>
2023-05-29 16:09:36 -0300

    Feat: fetching data with useQuery and previous / next page working

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
 import { useEffect, useState } from 'react'
+import { useFetchAll } from './fetchApi';
+import { isError, useQuery } from '@tanstack/react-query';
 
 
 function App() {
   // const [url, setUrl] = useState('https://swapi.dev/api/people/');
   const [currPage, setCurrPage] = useState(1);
-  const [list, setList] = useState<Root>({
-    count: 0,
-    next: '',
-    previous: '',
-    results: [],
-  });
   const [valueSearch, setValueSearch] = useState('');
 
-  interface Root {
-    count: number,
-    next: string | null,
-    previous: string | null,
-    results: [],
-  }
+  const query = useFetchAll(currPage)
+
+  // const url = 'https://swapi.dev/api/people/?page=' + currPage;
+  // const urlSearch = 'https://swapi.dev/api/people/?search=' + valueSearch;
 
 
-  const url = 'https://swapi.dev/api/people/?page=' + currPage;
-  const urlSearch = 'https://swapi.dev/api/people/?search=' + valueSearch;
-
-  useEffect(() => {
-    if (valueSearch) {
-      fetch(urlSearch).then(resp => resp.json()).then((value) => setList(value))
-    } else {
-      fetch(url).then(resp => resp.json()).then((value) => setList(value))
-    }
-  }, [url, urlSearch])
+  console.log(query);
   
-  const finalPage = Math.ceil(list.count / 10)
-  const pagination = new Array(finalPage).fill('')
+  
+  // const finalPage = Math.ceil(query.data?.count / 10)
+  // const pagination = new Array(finalPage).fill('')
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -48,59 +34,64 @@ function App() {
       <div className=" flex flex-col items-center w-screen h-screen text-yellow-400 gap-4">
         <div className="navbar bg-base-300 flex justify-center">
           <a className="btn btn-ghost Uppercase text-3xl">Star Wars</a>
-          {/* <div>{currPage}</div> */}
+          <div>{currPage}</div>
         </div>
 
-        <form method='post' onSubmit={handleSubmit} className="flex gap-5">
+        <form method="post" onSubmit={handleSubmit} className="flex gap-5">
           <input
-            name='search'
+            name="search"
             type="text"
             placeholder="Search"
             className="input input-bordered"
           />
-          <button type='submit' className=' btn btn-outline'>Pesquisar</button>
+          <button type="submit" className=" btn btn-outline">
+            Pesquisar
+          </button>
         </form>
 
-        <div className="overflow-x-auto w-2/3">
-          <table className="table w-full">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>Number</th>
-                <th className="">Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {list.results.map((person: { name: string; url: string }) => (
-                <tr key={person.name} className=" hover">
-                  <th>{person.url.match(/\d+/)}</th>
-                  <td>{person.name}</td>
+        {query.isLoading ? (
+          <div>Loading...</div>
+        ) : query.isError ? (
+          <div>Erro: {query.error.message}</div>
+        ) : (
+          <div className="overflow-x-auto w-2/3">
+            <table className="table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Number</th>
+                  <th className="">Name</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {query.data.results.map(
+                  (person: { name: string; url: string }) => (
+                    <tr key={person.name} className="hover">
+                      <th>{person.url.match(/\d+/)}</th>
+                      <th>{person.name}</th>
+                    </tr>
+                  )
+                )}
+              </tbody>
+            </table>
+          </div>
+        )}
+
         <div className="btn-group flex rounded-s-none">
           <div>
             <button
-              onClick={() => (currPage > 1 ? setCurrPage(currPage - 1) : null)}
+              onClick={() => setCurrPage((old) => Math.max(old - 1, 0))}
+              disabled={currPage === 1}
               className="btn btn-outline border-yellow-400 text-yellow-400 w-40"
             >
               Previous
             </button>
-            {pagination.map((_item, index) => (
-              <button
-                key={index}
-                onClick={() => setCurrPage(index + 1)}
-                className="btn btn-outline border-yellow-400 text-yellow-400 rounded-t-none"
-              >
-                {index + 1}
-              </button>
-            ))}
+            {}
             <button
-              onClick={() =>
-                finalPage === currPage ? null : setCurrPage(currPage + 1)
+              onClick={() => setCurrPage((old) => old + 1)
+                
               }
+              disabled={!query.data.next}
               className="btn btn-outline border-yellow-400 text-yellow-400 w-40"
             >
               Next
--- src/fetchApi.ts
+++ src/fetchApi.ts
@@ -1,18 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 
-export function useFetchAll(page = 0) {
-    const {
-        isLoading,
-        isError,
-        error,
-        data,
-        isFetching,
-        isPreviousData,
-    } = useQuery({
+export function useFetchAll(page = 1) {
+    return useQuery({
         queryKey: ['people', page],
-        queryFn: () => fetch('https://swapi.dev/api/people/?page=' + page)
-                        .then(res => res.json()),
-        keepPreviousData: true
+        queryFn: async () => {
+            const response = await fetch('https://swapi.dev/api/people/?page=' + page)
+            if (!response.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return response.json()
+        },
+        keepPreviousData: true,
     })
-    return { isLoading, isError, error, data, isFetching, isPreviousData }
 }
