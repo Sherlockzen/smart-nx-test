a9e75085f297d6f84eec42234655c8f57e21272e
Hewerton <hewertonlourenco@gmail.com>
2023-05-30 20:30:46 -0300

    Refactor: create useSearchPeople hook file

---
+++ src/hooks/useSearchPeople.tsx
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query"
+
+export function useFetchSearch(search = '') {
+    return useQuery({
+        queryKey: ['people', search],
+        queryFn: async () => {
+            const response = await fetch('https://swapi.dev/api/people/?search=' + search)
+            if (!response.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return response.json()
+        },
+    })
+}
