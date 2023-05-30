c4e438e7af52dba19107fd8d5c507269671a2437
Hewerton <hewertonlourenco@gmail.com>
2023-05-30 20:30:26 -0300

    Refactor: create useAllPeople hook file

---
+++ src/hooks/useAllPeople.tsx
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query"
+
+export function useFetchAll(page = 1) {
+    return useQuery({
+        queryKey: ['people', page],
+        queryFn: async () => {
+            const response = await fetch('https://swapi.dev/api/people/?page=' + page)
+            if (!response.ok) {
+                throw new Error('Network response was not ok')
+            }
+            return response.json()
+        },
+        keepPreviousData: true,
+    })
+}
